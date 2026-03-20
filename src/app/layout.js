b5bb8f3b77f0d5814870e2d19f2f14166b7d65ab
João Lucas <joao.lgomes1166@gmail.com>
2026-03-20 16:06:29 -0300

    import: speed-insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
 import { Syne, DM_Sans } from "next/font/google";
 import "./globals.css";
