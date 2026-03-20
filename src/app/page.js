d3b66a38f5887ebf4f80c6c0a100f6856c5282a7
João Lucas <joao.lgomes1166@gmail.com>
2026-03-20 14:41:20 -0300

    fix: error to see landing

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
+import Features from '@/components/Features';
 import FloatingOrbs from '@/components/FloatingOrbs';
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen bg-black">
+    <main className="relative min-h-screen bg-black text-white">
       <FloatingOrbs />
       <Navbar />
       <div className="relative z-10">
         <Hero />
+        <Features />
       </div>
     </main>
   );
